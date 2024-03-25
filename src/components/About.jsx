7890c8e8bd558ef75ec29303d4366c0212c2ae8c
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Pro.local>
2024-03-25 17:59:01 -0600

    responsiveness tweaks + add replace lopum ipsum texts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,26 +3,20 @@ import profPic from '../assets/profPic.jpeg';
 
 function About() {
   return (
-    <section className="h-screen flex flex-col justify-center items-center">
-      <div className="max-w-3xl mx-auto px-8">
-        
-        <div className="flex justify-center mb-8">
-          <img
-            src= {profPic}
-            alt="Developer"
-            className="w-48 h-48 rounded-full border-4 border-[#F3FFDB]"
-          />
-          
-        </div>
-        <h2 className="text-5xl font-bold mb-8 text-center">Hyrum Dolan</h2>
-        <div className="text-xl leading-relaxed mb-8">
-          <p className="mb-4">
-            Hi, I'm a web developer with experience in building single-page applications using React. I have a passion for creating responsive and user-friendly websites.
-          </p>
-          <p className="mb-4">
-            With a strong foundation in front-end technologies like HTML, CSS, and JavaScript, I strive to deliver high-quality code and seamless user experiences. I enjoy tackling complex challenges and finding elegant solutions to meet the needs of both clients and end-users.
-          </p>
-        </div>
+    <section className="min-h-screen flex flex-col justify-center items-center px-4 py-8"> <div className="max-w-3xl mx-auto w-full"> <div className="flex justify-center mb-8"> <img src={profPic} alt="Developer" className="w-1/2 sm:w-1/3 md:w-1/4 rounded-full border-4 border-[#F3FFDB]" /> </div>
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-8 text-center">Hyrum Dolan</h2>
+
+<div className="text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed mb-8">
+  <p>Hi friend,</p>
+
+  <p className="mb-4">
+    I'm a full-stack developer passionate about automation, UX, and Generative AI. I create tools to help people work smarter, utilizing my diverse experience with various languages and technologies. I constantly seek new challenges and opportunities to learn and grow, believing in technology's power to improve lives.
+  </p>
+
+  <p className="mb-4">
+    With a background in sales and management, I led a team of nearly 30 reps, generating $4 million in revenue within 5 months. I transitioned to tech to pursue a career that allows me to make significant impacts by developing tools and systems that help businesses grow quickly and enhance employee satisfaction and productivity.
+  </p>
+</div>
         
       </div>
     </section>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,45 +3,45 @@ import React from 'react';
 function Portfolio() {
   const projects = [
     {
-      title: 'React Portfolio (This Website)',
-      description: 'A responsive portfolio website built with React, focused on simple responsive design that uses Tailwind for styling.',
+      title: `React Portfolio (This)`,
+      description: 'Built a responsive portfolio website using React and Tailwind CSS.',
       repoLink: 'https://github.com/hyrumsdolan/React-Portfolio',
       demoLink: '#NeedToDeploy',
     },
     {
-      title: 'Project 2',
-      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      repoLink: '#project2',
-      demoLink: '#demo2',
+      title: 'AI Pokemon Battler',
+      description: 'Using lua to retrieve pokemon data and give to ChatGPT API to allow it to play pokemon.',
+      repoLink: 'https://github.com/hyrumsdolan/AI-Pokemon_battler',
+      demoLink: '#NeedToRecord',
     },
     {
-      title: 'Project 3',
-      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      repoLink: '#project3',
-      demoLink: '#demo3',
+      title: 'Simple JS/CSS Game',
+      description: 'Early learning project to better understand CSS animations. Used array of arrays to track game state. (use WASD for demo)',
+      repoLink: 'https://github.com/hyrumsdolan/Basic-Javascript-Game',
+      demoLink: 'https://hyrumsdolan.github.io/Basic-Javascript-Game/',
     },
     {
-      title: 'Project 4',
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      repoLink: '#project4',
-      demoLink: '#demo4',
+      title: 'Fitness Tracker',
+      description: 'Group project where I led a team of 3 to build a fitness tracker using the MVC design pattern. Using Node, Express, MySQl, Mongoose, and Handlebars.',
+      repoLink: 'https://github.com/hyrumsdolan/fitness-tracker-MVC',
+      demoLink: 'https://workout-fitness-tracker-ff2f5d2f6a1e.herokuapp.com/login',
     },
     {
-      title: 'Project 5',
-      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      repoLink: '#project5',
-      demoLink: '#demo5',
+      title: 'Custom Map Plotter',
+      description: 'Found a need at work for a flexible ad quick map plotter. Built utilizing Google Maps API allowing bulk plotting of locations from a CSV.',
+      repoLink: 'https://github.com/hyrumsdolan/Custom-Map-Plotter',
+      demoLink: 'https://hyrumsdolan.github.io/Custom-Map-Plotter/',
     },
     {
-      title: 'Project 6',
-      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-      repoLink: '#project6',
+      title: 'Q-Learning Snake',
+      description: 'Snake game made with PyGame using Q-Tables to enable learning and AI-play. Learning project for refreshing on Python OOP and Q-learning.',
+      repoLink: 'https://github.com/hyrumsdolan/snake-Q-learning',
       demoLink: '#demo6',
     },
   ];
 
   return (
-    <section className="min-h-screen py-16 flex flex-col justify-center items-center text-[#F3FFDB]">
+    <section className="min-h-screen flex flex-col justify-center items-center text-[#F3FFDB]">
       <div className="max-w-full mx-auto">
         <h2 className="text-5xl font-bold mb-12 text-center">Portfolio</h2>
         <div className="overflow-x-auto pl-4">
@@ -53,8 +53,8 @@ function Portfolio() {
                   className="inline-block bg-[#2b2b2b] border border-[#eceff133] rounded-2xl overflow-hidden flex flex-col w-80 text-wrap"
                 >
                   <div className="p-6 flex-grow">
-                    <h3 className="text-2xl font-semibold mb-4">{project.title}</h3>
-                    <p className="text-lg mb-6 line-clamp-4">{project.description}</p>
+                    <h3 className="text-2xl font-semibold mb-4 " style={{ whiteSpace: 'pre-wrap' }}>{project.title}</h3>
+                    <p className="text-lg mb-6 line-clamp-6">{project.description}</p>
                   </div>
                   <div className="border-t border-[#eceff133] mt-auto flex">
                     <a
